ea7d8f808a0d43d79dd70c270c773badc97c058e
Blxucreep <LoevanLqc@outlook.com>
2025-01-16 04:03:36 +0100

    supplier_command becomes manufacture with new functionalities

--- vnergy/plm/urls.py
+++ vnergy/plm/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import logout, login, signup, home, dashboard, supplier, supplier_add, supplier_delete, supplier_command, supplier_order, feedback, feedback_answer
+from .views import logout, login, signup, home, dashboard, sale, sale_update_status, supplier, supplier_add, supplier_delete, manufacture, supplier_order, manufacture_item, feedback, feedback_answer
 
 urlpatterns = [
     path('login/', login, name='login'),
@@ -10,12 +10,16 @@ urlpatterns = [
 
     path('dashboard/', dashboard, name='dashboard'),
 
+    path('sale/', sale, name='sale'),
+    path('sale_update_status/<int:order_id>/', sale_update_status, name='sale_update_status'),
+
     path('supplier/', supplier, name='supplier'),
     path('supplier/add/', supplier_add, name='supplier_add'),
     path('supplier/delete/<int:supplier_id>/', supplier_delete, name='supplier_delete'),
 
-    path('supplier_command/', supplier_command, name='supplier_command'),
+    path('manufacture/', manufacture, name='manufacture'),
     path('supplier_order/<int:supplier_id>/<int:item_id>/', supplier_order, name='supplier_order'),
+    path('manufacture_item/<int:item_id>/', manufacture_item, name='manufacture_item'),
 
     path('feedback/', feedback, name='feedback'),
     path('feedback/<int:feedback_id>/', feedback_answer, name='feedback_answer'),
--- vnergy/plm/views.py
+++ vnergy/plm/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import JsonResponse
-from .models import Orders, Admins, Suppliers, IsSupplied, CustomerFeedbacks
+from .models import Orders, Items, IsComposed, Stocked, Admins, Suppliers, IsSupplied, CustomerFeedbacks
 from datetime import datetime
 import json
+import random
+from collections import defaultdict
 
 def logout(request):
     # set the cookie to expire
@@ -112,6 +113,80 @@ def dashboard(request):
     return render(request, 'dashboard.html', {'username': username, 'data': json.dumps(data_converted), 'x_axis': x, 'y_axis': y})
 
 
+def sale(request):
+    username = request.COOKIES.get('username', None)
+    orders = Orders.objects.all()
+    
+    # get the items for each order
+    orders_with_items = []
+    for order in orders:
+        items = IsComposed.objects.filter(order_id=order).select_related('item_id')
+        items_details = [
+            {
+                'name': item.item_id.name,
+                'price': item.item_id.price,
+                'quantity': item.quantity
+            }
+            for item in items
+        ]
+        orders_with_items.append({
+            'order_id': order.order_id,
+            'order_date': order.order_date,
+            'email': order.email.email,
+            'total_price': order.total_price,
+            'items': items_details,
+            'status': order.status
+        })
+    
+    # group by status
+    orders_by_status = defaultdict(list)
+    for entry in orders_with_items:
+        orders_by_status[entry['status']].append(entry)
+
+    # re-convert the defaultdict to a dict
+    orders_by_status = dict(orders_by_status)
+
+    # re-organize the dict to have the status in this order: pending, confirmed, in transit, delivered, cancelled
+    orders_by_status = sorted(orders_by_status.items(), key=lambda x: ['Pending', 'Confirmed', 'In Transit', 'Delivered', 'Cancelled'].index(x[0]))
+    orders_by_status = dict(orders_by_status)
+
+    return render(request, 'sale.html', {'username': username, 'orders_by_status': orders_by_status})
+
+def get_random_warehouse_for_item(item):
+    # get the specific warehouses that stock the item
+    warehouses_with_item = Stocked.objects.filter(item_id=item.item_id)
+
+    if warehouses_with_item.exists():
+        # select a random warehouse that stocks the item
+        random_warehouse = random.choice(warehouses_with_item)
+        return random_warehouse
+    else:
+        return None
+
+def sale_update_status(request, order_id):
+    order = Orders.objects.get(order_id=order_id)
+
+    if order.status != 'Delivered' and order.status != 'Cancelled':
+        if order.status == 'Pending':
+            # update the stock of the items
+            items = IsComposed.objects.filter(order_id=order).select_related('item_id')
+            for item in items:
+                warehouse = get_random_warehouse_for_item(item)
+                if warehouse:
+                    stock = Stocked.objects.get(item_id=item.item_id, warehouse_id=warehouse.warehouse_id)
+                    stock.quantity -= item.quantity
+                    stock.save()
+                else:
+                    continue
+        # update the status of the order
+        statuses = ['Pending', 'Confirmed', 'In Transit', 'Delivered']
+        current_index = statuses.index(order.status)
+        order.status = statuses[current_index + 1]
+        order.save()
+
+    return redirect('sale')
+
+
 def supplier(request):
     username = request.COOKIES.get('username', None)
     suppliers = Suppliers.objects.all()
@@ -137,21 +212,21 @@ def supplier_delete(request, supplier_id):
     return redirect('supplier')
 
 
-def supplier_command(request):
+def manufacture(request):
     username = request.COOKIES.get('username', None)
 
     # get all the relations between suppliers and items
     supplies = IsSupplied.objects.select_related('supplier_id', 'item_id')
 
-    supplies_with_names = []
+    supplies_with_infos = []
     for supply in supplies:
         supplier = supply.supplier_id
         item = supply.item_id
 
         supply = IsSupplied.objects.filter(supplier_id=supplier, item_id=item)
 
-        # add the total stock of the item
-        supplies_with_names.append({
+        # add the supply to the list
+        supplies_with_infos.append({
             'product_name': supplier.product_sold,
             'item_id': item.item_id,
             'item_name': item.name,
@@ -160,7 +235,33 @@ def supplier_command(request):
             'quantity': supply[0].quantity or 0
         })
 
-    return render(request, 'supplier_command.html', {'username': username, 'supplies_with_names': supplies_with_names})
+    items = Items.objects.all()
+
+    cans = []
+    for item in items:
+        item_id = item.item_id
+        name = item.name
+
+        # get the ingredients of the item
+        ingredients = IsSupplied.objects.filter(item_id=item_id)
+        
+        # get the names of the ingredients
+        ingredients_with_names = Suppliers.objects.filter(supplier_id__in=[ingredient.supplier_id.supplier_id for ingredient in ingredients])
+        product_names = ', '.join([ingredient.product_sold for ingredient in ingredients_with_names])
+
+        # get the quantity of the cans
+        can = Stocked.objects.filter(item_id=item_id)
+        quantity = can[0].quantity if can else 0
+
+        # add the can to the list
+        cans.append({
+            'item_id': item_id,
+            'name': name,
+            'ingredients': product_names,
+            'quantity': quantity
+        })
+
+    return render(request, 'manufacture.html', {'username': username, 'supplies_with_infos': supplies_with_infos, 'cans': cans})
 
 def supplier_order(request, supplier_id, item_id):
     # get the item supplied by the supplier
@@ -169,7 +270,22 @@ def supplier_order(request, supplier_id, item_id):
     supplied_item.quantity += 10
     supplied_item.save()
 
-    return redirect('supplier_command')
+    return redirect('manufacture')
+
+def manufacture_item(request, item_id):
+    # get the supplies
+    supplies = IsSupplied.objects.filter(item_id=item_id)
+
+    for supply in supplies:
+        supply.quantity -= 1
+        supply.save()
+
+    # get the can
+    can = Stocked.objects.get(item_id=item_id)
+    can.quantity += 1
+    can.save()
+
+    return redirect('manufacture')
 
 
 def feedback(request):
